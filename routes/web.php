4f7db1294172e7a20e1dfa9d2d4a4651acacffa2
João Heitor <78935085+jhmartins1@users.noreply.github.com>
2023-08-21 20:49:41 -0300

    chore: change base routo to /

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use App\Http\Controllers\ClienteController;
 |
 */
 
-Route::get('clientes', [ClienteController::class, 'index'])->name('clientes.index');
+Route::get('/', [ClienteController::class, 'index'])->name('clientes.index');
 Route::get('cliente/datatable', [ClienteController::class, 'datatable'])->name('cliente.datatable');
