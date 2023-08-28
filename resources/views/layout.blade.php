d798cdf037132ebfacaa9aef83ef77dec21fb4b3
João Heitor <78935085+jhmartins1@users.noreply.github.com>
2023-08-28 19:52:38 -0300

    chore: add favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
+    {{-- add favicon --}}
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    {{-- add bootstrap --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
